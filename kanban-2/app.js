4d37e075e724162592b99959f6abdabe6a8abc7d
Karan <risingphoenix160@gmail.com>
2023-06-29 18:25:09 +0530

    feat: allow cards to be updated via md editor

--- kanban-2/app.js
+++ kanban-2/app.js
@@ -6,9 +6,11 @@
   const $addBtn = document.getElementById('add');
   const $submitBtn = document.getElementById('save');
   const $cancelBtn = document.getElementById('cancel');
-  const $cardDeck = document.getElementById('card-deck');
-  $mdEditor.value = `# Add Main Title Here\n## Add Sub-topic here\nThis is for adding paragraphs. Use this format to add content. In short use markdown.`
-  let formatted = '<div class="bg-[#5497C5] flex justify-between items-center px-4 py-2 border-b-4 border-gray-100"><h2 class="text-base text-white font-bold uppercase">Add Main Title Here</h2><i class="fa-solid fa-ellipsis" aria-hidden="true"></i></div><article class="text-sm px-4 py-2"><h3 class="text-base font-semibold leading-tight">Add Sub - topic here</h3><p>This is for adding paragraphs. Use this format to add content. In short use markdown.</p></article>';
+  let $editBtns = document.querySelectorAll('.edit-btn');
+
+  let formatted = '';
+  let $currentEdit = null;
+  let isEditing = false;
 
   $mdEditor.oninput = (e) => handleChange(e);
 
@@ -16,31 +18,61 @@
 
   $submitBtn.onclick = (e) => {
     e.preventDefault();
-    addCard(formatted);
-    $form.style.display = 'none';
+    if (!isEditing && $currentEdit === null) {
+      addCard(formatted);
+    } else {
+      $currentEdit.innerHTML = formatted;
+      $currentEdit = null;
+      isEditing = false;
+    }
+    closeForm();
   };
 
-  $addBtn.onclick = () => {
+  $addBtn.onclick = openForm;
+
+  $newColBtn.onclick = openForm;
+
+  $cancelBtn.onclick = closeForm;
+  
+  $editBtns.forEach($editBtn => {
+    $editBtn.onclick = (e) => editCard(e);
+  });
+  
+  function openForm() {
+    formatted = '<div class="bg-[#5497C5] flex justify-between items-center px-4 py-2 border-b-4 border-gray-100"><h2 class="text-base text-white font-bold uppercase">Add Main Title Here</h2><i class="fa-solid fa-ellipsis" aria-hidden="true"></i></div><article class="text-sm px-4 py-2"><h3 class="text-base font-semibold leading-tight mb-2">Add Sub - topic here</h3><p>This is for adding paragraphs. Use this format to add content. In short use markdown.</p></article>';
+    $mdEditor.value = '# Add Main Title Here\n## Add Sub-topic here\nThis is for adding paragraphs. Use this format to add content. In short use markdown.';
     $form.style.display = "flex";
-  };
+  }
 
-  $newColBtn.onclick = () => {
-    $form.style.display = "flex";
-  };
-
-  $cancelBtn.onclick = () => {
+  function closeForm() {
     $form.style.display = "none";
-    formatted = '<div class="bg-[#5497C5] flex justify-between items-center px-4 py-2 border-b-4 border-gray-100"><h2 class="text-base text-white font-bold uppercase">Add Main Title Here</h2><i class="fa-solid fa-ellipsis" aria-hidden="true"></i></div><article class="text-sm px-4 py-2"><h3 class="text-base font-semibold leading-tight">Add Sub - topic here</h3><p>This is for adding paragraphs. Use this format to add content. In short use markdown.</p></article>';
-  };
+    $mdEditor.value = '';
+    formatted = '';
+  }
+
+  function editCard(event) {
+    isEditing = true;
+    $currentEdit = event.currentTarget.nextElementSibling;
+    const subtitle = $currentEdit.querySelector('h3');
+    const paras = $currentEdit.querySelectorAll('p');
+    $mdEditor.value = `## ${subtitle.innerText}`;
+    formatted = `<h3 class="text-base font-semibold leading-tight mb-2">${subtitle.innerText}</h3>`;
+    paras.forEach((para, index) => {
+      $mdEditor.value += `\n${para.innerText}`;
+      formatted += `<p>${para.innerText}</p>`;
+    });
+    $form.style.display = "flex";
+  }
 
   function handleChange(event) {
     const sentences = event.target.value.split('\n');
     formatted = sentences.map(sentence => {
       if (sentence.startsWith('#')) {
+        if (sentence.trim() === '') return '';
         return addHeading(sentence);
       }
       return '<p>' + sentence + '</p>'
-    }).join('');
+    }).join('\n');
   }
 
   function addHeading(string) {
@@ -49,7 +81,7 @@
         return '<div class="bg-[#5497C5] flex justify-between items-center px-4 py-2 border-b-4 border-gray-100"><h2 class="text-base text-white font-bold uppercase">' + string.replace(/#+ /g, '') + '</h2><i class="fa-solid fa-ellipsis" aria-hidden="true"></i></div >\n';
 
       case 2:
-        return '\n<h3 class="text-base font-semibold leading-tight">' + string.replace(/#+ /g, '') + '</h3>';
+        return '\n<h3 class="text-base font-semibold leading-tight mb-2">' + string.replace(/#+ /g, '') + '</h3>';
 
       case 3:
         return '<h4>' + string.replace(/#+ /g, '') + '</h4>';
@@ -64,6 +96,13 @@
 
   function addCard($content) {
     const $card = document.createElement('div');
+    // Todo: add edit functionality to new ones
+    // const $editBtn = document.createElement('button');
+    // $editBtn.className = "edit-btn text-[#B4B6C1] px-4 mt-2";
+    // $editBtn.setAttribute('title', 'edit');
+    // $editBtn.innerHTML = '<i class="fa-solid fa-ellipsis" aria-hidden="true"></i>';
+    // $editBtn.onclick = (e) => editCard(e);
+    // Todo: add edit functionality to new ones
     $card.className = "bg-white rounded-md overflow-hidden shadow-md shadow-black/20 pb-4";
     $content = $content.split('\n\n')[0] + $content.split('\n\n').slice(1).map(item => '<article class="text-sm px-4 py-2">' + item + '</article>').join('');
     $card.innerHTML = $content;
