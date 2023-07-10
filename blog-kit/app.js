624c17f6128b457183217c76909a657d397395ed
Karan <risingphoenix160@gmail.com>
2023-07-10 16:57:22 +0530

    Update docs and refactor

--- blog-kit/app.js
+++ blog-kit/app.js
@@ -2,8 +2,42 @@ const $cursor = document.getElementById('cursor');
 const $cursorBlur = document.getElementById('cursor-blur');
 const $hoverables = document.querySelectorAll('[data-hoverable]');
 
+const $bookmarkBtns = document.querySelectorAll('.bookmark-btn');
+
 const themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
 const themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+
+document.onmousemove = function (e) {
+  $cursor.style.left = `${e.x - ($cursor.clientWidth / 2)}px`;
+  $cursorBlur.style.left = `${e.x - ($cursorBlur.clientWidth / 2)}px`;
+  $cursor.style.opacity = '1';
+  $cursor.style.top = `${e.y - ($cursor.clientWidth / 2)}px`;
+  $cursorBlur.style.top = `${e.y - ($cursorBlur.clientWidth / 2)}px`;
+  $cursorBlur.style.opacity = '1';
+}
+$hoverables.forEach($hoverable => {
+  $hoverable.onmouseover = function () {
+    $cursor.style.backgroundColor = 'transparent'
+    $cursor.style.transform = 'scale(3)';
+    $cursor.style.border = `1px solid ${document.documentElement.classList.contains('dark') ? 'white' : 'black'}`;
+  }
+  $hoverable.onmouseout = function () {
+    $cursor.style.backgroundColor = 'rgb(147 51 234)'
+    $cursor.style.transform = 'scale(1)';
+    $cursor.style.border = 'none';
+  }
+});
+
+$bookmarkBtns.forEach($bookmarkBtn => {
+  $bookmarkBtn.onclick = function (e) {
+    const $icon = e.currentTarget.querySelector('i');
+    if ($icon.classList.contains('fa-solid')) {
+      $icon.classList.replace('fa-solid', 'fa-regular');
+    } else {
+      $icon.classList.replace('fa-regular', 'fa-solid');
+    }
+  }
+});
 
 if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
   themeToggleLightIcon.classList.remove('hidden');
@@ -36,25 +70,4 @@ themeToggleBtn.addEventListener('click', function () {
     }
   }
 
-});
-
-document.onmousemove = function (e) {
-  $cursor.style.left = `${e.x - ($cursor.clientWidth / 2)}px`;
-  $cursorBlur.style.left = `${e.x - ($cursorBlur.clientWidth / 2)}px`;
-  $cursor.style.opacity = '1';
-  $cursor.style.top = `${e.y - ($cursor.clientWidth / 2)}px`;
-  $cursorBlur.style.top = `${e.y - ($cursorBlur.clientWidth / 2)}px`;
-  $cursorBlur.style.opacity = '1';
-}
-$hoverables.forEach($hoverable => {
-  $hoverable.onmouseover = function () {
-    $cursor.style.backgroundColor = 'transparent'
-    $cursor.style.transform = 'scale(2.5)';
-    $cursor.style.border = `0.4px solid ${document.documentElement.classList.contains('dark') ? 'white' : 'black'}`;
-  }
-  $hoverable.onmouseout = function () {
-    $cursor.style.backgroundColor = 'rgb(147 51 234)'
-    $cursor.style.transform = 'scale(1)';
-    $cursor.style.border = 'none';
-  }
 });
