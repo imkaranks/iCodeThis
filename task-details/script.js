d2569b94d0866f4bfb08380c8b33e9f7183b9dbb
Karan <risingphoenix160@gmail.com>
2023-09-02 18:18:13 +0530

    Tweak stylings and transitions

--- task-details/script.js
+++ task-details/script.js
@@ -6,12 +6,21 @@
   const $progressInput = document.getElementById('progress');
   const $attachmentInput = document.getElementById('attachment');
 
+  setTimeout(() =>
+    updateProgress($progressInput.value)
+  , 0);
+
   $progressInput.oninput = function (e) {
     const val = e.target.value;
-    if (val > 100) {
+    if (val > 100 || val < 0) {
       alert('Value must be between 0 and 100');
+      e.target.value = val.slice(0, val.length-1);
       return;
     }
+    updateProgress(val);
+  }
+
+  function updateProgress(val) {
     $progressBar.setAttribute('style', `--percentage: ${val}%`);
   }
 
