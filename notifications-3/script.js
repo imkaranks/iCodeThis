71a408cb0906d3cf9506f5b9794f3990f6c72887
Karan <risingphoenix160@gmail.com>
2023-09-03 23:10:18 +0530

    Add animations and hover effects

--- notifications-3/script.js
+++ notifications-3/script.js
@@ -1,4 +1,40 @@
-document.onclick = function (e) {
-  if (e.target.closest('button'))
-    alert("Oh c'mon!, it's sunday. Let me have some rest😉");
-}
+'use strict';
+
+(function () {
+  const $notificationToggle = document.querySelector('[data-new-notification]');
+  const $notifications = document.querySelectorAll('[data-notification]');
+
+  document.onclick = function (e) {
+    if (e.target.closest('#notification-toggle')) {
+      const $parentWrapper = document.getElementById('notifications-wrapper');
+      $parentWrapper.setAttribute(
+        'aria-hidden',
+        $parentWrapper.getAttribute('aria-hidden') === 'true'
+          ? false
+          : true
+      );
+      $notifications.forEach(($notification, i) => {
+        $notification.classList.toggle('animate-entry');
+        $notification.setAttribute('style', `--delay: ${i}`);
+      });
+    } else if (e.target.closest('[data-mark-read]')) {
+      if ($notificationToggle.getAttribute('data-new-notification') === 'false') return;
+      markAllAsRead();
+    } else if (e.target.closest('[data-accept]')) {
+      const $btnWrapper = e.target.closest('[data-accept]').parentNode;
+      $btnWrapper.innerHTML = `<p class="text-sm font-normal text-green-300">Accepted</p>`;
+    } else if (e.target.closest('[data-decline]')) {
+      const $btnWrapper = e.target.closest('[data-decline]').parentNode;
+      $btnWrapper.innerHTML = `<p class="text-sm font-normal text-red-300">Declined</p>`;
+    }
+  }
+
+  function markAllAsRead() {
+    $notificationToggle.classList.remove('animate-bell');
+    $notificationToggle.dataset.newNotification = false;
+    $notificationToggle.dataset.notificationCount = 0;
+    $notifications.forEach($notification => {
+      $notification.dataset.new = false;
+    })
+  }
+})();
